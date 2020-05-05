d948cef178255be15491949d740a0604c126d052
gitlmcs <gitlmcs@gmail.com>
2020-05-05 00:32:12 -0500

    mating pool 2 o seleccion de padres

--- lab01.py
+++ lab01.py
@@ -55,23 +55,79 @@ fitness_arrays = Fitness(Initial_population)
 # seleccion por torneo
 print ("MATING POOL")
 
+def fwinner(p1, p2):
+    #gana el que tenga menor aptitud (minimizar funcion)
+    _p1 = f(p1[0],p1[1])
+    _p2 = f(p2[0], p2[1])
+    print("Aptitudes → ", _p1 , " «» ", _p2)
+    
+    if(_p1 < _p2):
+        return p1
+    else:
+        return p2
+    
+
 def mating_pool(population):
     #seleccion de 8 peleas y 16 competidores
     i = 0
+    new_selected = np.array([])
     while(i<Number_individuals):
-        r1 = random.randint(0,Number_individuals)
-        r2 = random.randint(0,Number_individuals)
+        r1 = random.randint(0,Number_individuals-1) #cuidado con sobrepasar la numero de invidiuos [0 a 7]
+        r2 = random.randint(0,Number_individuals-1)
         if(r1 != r2):
-            print("random: r1 y r2 ", r1, r2)
-            print("i", i)
+            print("Enfrentamiento: ", i)
+            print(r1, 'vs', r2)
+            print(population[r1,:] , " VS ", population[r2,:])
+            winner = fwinner(population[r1,:], population[r2,:])
+            print("Ganador → ", winner)
+            new_selected = np.append(new_selected, winner, 0)
+            print()
+            # print(population[r1,:])
+            # print(" VS ")
+            # print(population[r2,:])
+            # print("i", i)
+            i += 1        
         else:
             continue
-        i += 1        
+    
+    # print(population[0,:])
+    # print(population[8,:])
+            
 
-    print(population[0,:])
+    # print(population[0,:])
+    new_selected = np.reshape(new_selected, (Number_individuals, 2))
+    print(new_selected)
+    return new_selected
 
-mating_pool(Initial_population)
+# mating_pool(Initial_population)
+new = mating_pool(Initial_population)
 
+
+
+def mating_pool_2(candidates):
+    i = 0
+    parents = np.array([])
+    while(i < Number_individuals):
+        random1 = random.randint(0,Number_individuals-1)
+        random2 = random.randint(0,Number_individuals-1)
+        if(random1 != random2):
+            if(candidates[random1,0] != candidates[random2,0]):
+                print("PARENTS → ", random1, " <3 ", random2)
+                print(candidates[random1,:], " <3 ", candidates[random2,:])
+                parents = np.append(parents, np.array([candidates[random1,:], candidates[random2,:]]))
+                i += 1
+            else:
+                continue
+        else:
+            continue
+    return parents
+    
+
+def choose_parents(candidates):
+    a = mating_pool_2(candidates) 
+    print(a)
+    
+choose_parents(new)
 
 
 
