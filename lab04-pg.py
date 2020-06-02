9025fb046dda6dc8ba64232bf953028df8cff8fd
lewistec90 <gitlmcs@gmail.com>
2020-06-02 00:41:27 -0500

    avance de lab4 y muchos errores - no terminado

--- lab04-pg.py
+++ lab04-pg.py
@@ -48,7 +48,7 @@ terminal_var = ['X']
 prob_repro = 0.2
 prob_cruz = 0.4
 prob_mut = 0.4
-
+generations = 10
 
 #  crear poblacion inicial
 #  evaluar aptitud
@@ -56,8 +56,14 @@ prob_mut = 0.4
 #  termimna el bucle
 
 
+print('TAMAÑO DE LA POBLACION: ', size_pop)
+print('TAMAÑO DE GENES', size_genes)
+print('PROBABILIDAD DE REPRODUCCIÒN/REPLICACIÒN: ', prob_repro*100)
+print('PROBABILIDAD DE CRUZAMIENTO: ', prob_cruz*100)
+print('PROBABILIDAD DE MUTACIÓN: ', prob_mut*100)
 print('FUNCIONES: ', funciones)
 print('TERMINALES: ', terminal_const + terminal_var)
+print('---------------------------------------------------------------')
 
 tableIO = [[0, 0],
            [0.1, 0.005],
@@ -69,6 +75,9 @@ tableIO = [[0, 0],
            [0.7, 0.245],
            [0.8, 0.32],
            [0.9, 0.405]]
+
+# print('TABLA INPUT - OUTPUT')
+# print(tableIO)
 
 
 class Arbol(object):
@@ -149,15 +158,14 @@ def generar_poblacion():
 
 
 # poblacion inicial
-pobl_ini = generar_poblacion()
+# pobl_ini = generar_poblacion()
 
 
 def convertir_a_funcion(ind, param):
-    """ convierte la secuencia del arbol en una función """
+    """ convierte la secuencia del arbol en una función y devuelve el resultado"""
     op = {'+': lambda a,b:a+b, '-': lambda a,b:a-b, '*': lambda a,b: a*b, '/':lambda a,b: a/b}
     if 'X' not in ind:
         return op[ind[3]] ( op[ind[1]] (int(ind[0]), int(ind[2])), op[ind[5]] (int(ind[4]), int(ind[6])))
-        # return op[ind[3]]( op[ind[1]](int(ind[0]), int(ind[2]))), op[ind[5]](int(ind[4]), int(ind[6]))
     else:
         indice = ind.index('X')
         if indice == 0:
@@ -170,32 +178,141 @@ def convertir_a_funcion(ind, param):
             return op[ind[3]] ( op[ind[1]] (int(ind[0]), int(ind[2])), op[ind[5]] (int(ind[4]), param))
 
 
-
-print(pobl_ini[0])
-print(convertir_a_funcion(pobl_ini[0], 1))
+print('***********************')
+# print(pobl_ini[0])
+# print(convertir_a_funcion(pobl_ini[0], 1))
 print('===================================')
-def aptitud(tabla, pop):
-    print('CALCULO DE APTITUDES')
-    n = len(tabla)
-    tab = tabla
-    for ii in range (len(pop)):
-        diffs = []
-        print('INDIVIDUO: ', ii)
-        for i in range (n):
-            actual = convertir_a_funcion(pop[ii], tab[i][0])
-            tab[i].append(actual)
-            diffs.append(tab[i][1] - actual)
-            tab[i].append(tab[i][1] - actual)
-        print('individuo - secuencia: ', pop[ii])
-        val = np.array(diffs)
-        ap = np.sum(np.power(diffs, 2)) / n
-        print('APTITUD = ', ap)
+# def aptitud(pop):
+#     print('------------------CALCULO DE APTITUDES')
+#     n = len(tableIO)
+#     m = len(pop)
+#     tab = tableIO
+#     for ii in range (m):
+#         diffs = []
+#         print('INDIVIDUO: ', ii)
+#         for i in range (n):
+#             pass
+#             # actual = convertir_a_funcion(pop[ii], tab[i][0])
+#             # print(actual)
+#             # tab[i].append(actual)
+#             # diffs.append(tab[i][1] - actual)
+#             # tab[i].append(tab[i][1] - actual)
+#         # print('individuo - secuencia: ', pop[ii])
+#         print(tab)
+#         # val = np.array(diffs)
+#         # ap = np.sum(np.power(diffs, 2)) / n
+# #         # print('APTITUD = ', ap)
+# aptitud(pobl_ini)
+
+def imprimir(p):
+    """ imprime poblaciones """
+    for item in p:
+        print('-> ', item, '\t\n')
+
+
+def aptitud(ind):
+    print('INICIO DEL CÁLCULO DE LA APTIRUD DEL INDIVIDUO')
+    n = len(tableIO)
+    tab = tableIO
+    actual = []
+    for i in range(n):
+        actual.append(convertir_a_funcion(ind, tableIO[i][0])) 
+    print(n)
+    print(actual)
+    for ii in range(len(tab)):
+        tab[ii].append(actual[ii])
+    # for j in actual:
+    #     tab = [row.append(j) for row in tab]
+    print(' AÑADIENDO ACTUALES A LA TABLA')
+    imprimir(tab)
+    ###################################
+    diffs = []
+    for k in range(len(tab)):
+        diffs.append((tab[k][1] - tab[k][2]))
+    print(diffs)
+    for y in range(len(tab)):
+        tab[y].append(diffs[y])
+    print(' AÑADIENDO LA DIFERENCIA ENTRE LA SALIDA ESPERADA Y LA SALIDA ACTUAL')
+    print('INPUT - ESPECTED - ACTUAL - DIFERENCE')
+    imprimir(tab)
+    numpy_tab = np.array(tab)
+    ECM = np.sum(numpy_tab[:,3]**2) # Utilizando la formula de ERROR CUADRATICO MEDIO
+    # print(ECM)
+    return ECM
     
 
-print(aptitud(tableIO, pobl_ini))
+def replicacion(pob, r):
+    print('INICIO DE REPLICACIÓN \t Probabilidad dada: ', r)
+    print('Seleccionar 3 para torneo')
+    fighters = [random.randint(0,8), random.randint(0,8), random.randint(0,8) ]
+    a1=aptitud(pob[fighters[0]])# ver
+    a2=aptitud(pob[fighters[1]])# ver
+    a3=aptitud(pob[fighters[2]])# ver
+    maxi = max([a1,a2,a3])
+    pob[pob.index(maxi)]
+    
+    return pob
+
+def cruzamiento(pob, r):
+    print('INICIO DE CRUZAMIENTO \t Probabilidad dada: ', r)
+    return pob
+
+def mutacion(pob, r):
+    print('INCICIO DE MUTACION \t Probabilidad dada: ', r)
+    return pob
+
+
+def calcular_aptitudes(pop):
+    print("CALCULAR APTITUDES")
+    print(aptitud(pop[0]))
+    print(aptitud(pop[1]))
+    print(aptitud(pop[2]))
+    print(aptitud(pop[3]))
+    print(aptitud(pop[4]))
+    print(aptitud(pop[5]))
+    print(aptitud(pop[6]))
+    print(aptitud(pop[7]))
+    
+
+    return pop
+
+def main():
+    pobl_ini = generar_poblacion()
+    print('===================================')
+    print("POBLACIÓN INICIAL", type(pobl_ini))
+    imprimir(pobl_ini)
+    print("TABLA INPUT-OUTPUT", type(tableIO))
+    imprimir(tableIO)
+    pobl_actual = pobl_ini
+    for i in range(1): #  generations 
+        print(' ------------------------------- ITERACIÓN N°: ', i)
+        # aptitudes = list(map(aptitud, pobl_actual))
+        pob_con_aptitudes = calcular_aptitudes(pobl_actual)
+        imprimir(pob_con_aptitudes)
+        j = 0
+        while j < len(pobl_actual):
+            prob = random.uniform(0, 1) 
+            if prob > 0.0 and prob < 0.2:
+                pobl_actual = replicacion(pobl_actual, prob) #revisar 
+                j += 1
+            elif prob > 0.2 and prob < 0.6:
+                if j == size_pop - 1:
+                    pobl_actual = mutacion(pobl_actual, prob)
+                else:
+                    pobl_actual =cruzamiento(pobl_actual, prob)
+                j += 2
+            elif prob > 0.6 and prob < 1.0:
+                pobl_actual = mutacion(pobl_actual, prob)
+                j += 1
+
+        print('NUEVA POBLACION')
+        imprimir(pobl_actual)
+    
+    print('POBLACION FINAL')
+    imprimir(pobl_actual)
 
 
 
-
+main()
 
 
