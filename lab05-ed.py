5c568d48a2adc9886161709578d0c500abacba41
lewistec90 <lewsdev@gmail.com>
2020-06-06 17:18:21 -0500

    evolucion diferencial terminado

--- lab05-ed.py
+++ lab05-ed.py
@@ -12,22 +12,36 @@
 #  Lo demás parámetros los puede definir Ud.
 
 # ------------------------------------------------------------------------------------------------
+#  Librerias
 import numpy as np
 import random
+
 # --------------------------------------------------------------------------------------------------
+#  Variables globales
 size_gens = 4
 cant_individuos = 12
 F_const = 0.6
 C_const = 0.5
-iters = 1
+iters = 100
 # --------------------------------------------------------------------------------------------------
+# poblacion incial
+# funcion f dada por el problema
 poblacion_inicial = [[random.uniform(-10, 10) for gen in range(0, size_gens)] for row in range(cant_individuos)]
-
+# ejemplo de individuo: [5.218463460594267, -8.229967436387987, 5.692386759420723, -6.829114399698888]
 f = lambda x, y, a, b: (x + 2 * y - 7) ** 2 + (2 * x + y - 5) ** 2 + (a + 2 * b - 7) ** 2 + (2 * a + b - 5) ** 2
+
+
 # --------------------------------------------------------------------------------------------------
 
 
-def imprimir(pobl):
+# imprime poblacion incial
+def imprimir(pobl, apt=None):
+    if apt is not None:
+        print('APT IS NOT NONE')
+        for i in range(cant_individuos):
+            print(pobl[i], '\t', apt[i])
+        print()
+        return  # no muy seguro si es efectivo
     for row in pobl:
         print(row)
     print()
@@ -50,16 +64,88 @@ def titulos(func):
     print('---------------------------------')
 
 
+# vector de las aptitudes
 def aptitudes(pobl):
+    aptis = []
     for indi in pobl:
-        print(indi)
+        aptis.append(f(indi[0], indi[1], indi[2], indi[3]))
+    return aptis
 
 
+#  vector de mutacion
+def mutaciones(pop, xm, xk, xl):
+    matriz = np.array(pop)
+    diff = matriz[xk, :] - matriz[xl, :]
+    diff_pond = F_const * diff
+    diff_mut = matriz[xm] + diff_pond
+    print('VECTOR DE DIFERENCIAS: (Xk - Xl)')
+    print(diff)
+    print('VECTOR DE MUTACIÓN PONDERADA: F * (Xk - Xl)')
+    print(diff_pond)
+    print('VECTOR DE MUTACIÓN: Xm + F * (Xk - Xl')
+    print(diff_mut)
+    return list(diff_mut)
+
+
+# vector de cruzamiento, retorna un vector trial
+def cruzamiento(pop, mut, target,  cx, cy, ca, cb):
+    trial_vector = []
+    if cx > C_const:
+        trial_vector.append(pop[target][0])
+    else:
+        trial_vector.append(mut[0])
+    if cy > C_const:
+        trial_vector.append(pop[target][1])
+    else:
+        trial_vector.append(mut[1])
+    if ca > C_const:
+        trial_vector.append(pop[target][2])
+    else:
+        trial_vector.append(mut[2])
+    if cb > C_const:
+        trial_vector.append(pop[target][3])
+    else:
+        trial_vector.append(mut[3])
+    print('VECTOR TRIAL')
+    print(trial_vector)
+    return trial_vector
+
+
+# comparar vector actual vs trial -> los fitness
+def compare(actual, trial, target):
+    fvar_target = f(actual[target][0], actual[target][1], actual[target][2], actual[target][3])
+    fvar_trial = f(trial[0], trial[1], trial[2], trial[3])
+    if  fvar_target < fvar_trial :
+        print('--> --> --> Se mantiene el vector target para la nueva población')
+        return actual[target]
+    else:
+        print('--> --> --> El vector target es reemplazado por el vector trial en la nueva población')
+        return trial
+
+
+# funcion principal
 @titulos
 def main():
     print('POBLACIÓN INICIAL')
     imprimir(poblacion_inicial)
     print('CALCULAR LA APTITUD DE CADA INDIVIDUO')
-    aptitudes(poblacion_inicial)
+    imprimir(poblacion_inicial, aptitudes(poblacion_inicial))
+    poblacion_actual = poblacion_inicial
     for i in range(iters):
-        print('---> ITERACIÓN N° ', i)
+        poblacion_nueva = []
+        print('--> ITERACIÓN N° ', i)
+        for j in range(cant_individuos):  # vector 1, vector 2 ... vector j+1
+            print('--> --> VECTOR: ', j)
+            Xm, Xk, Xl = random.randint(0, cant_individuos - 1), random.randint(0, cant_individuos - 1), random.randint(0, cant_individuos - 1)
+            cr_x, cr_y, cr_a, cr_b = random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1)
+            print('FASE DE MUTACIÓN: Xm = %d, Xk = %d, Xl = %d ' % (Xm, Xk, Xl))
+            vector_muts = mutaciones(poblacion_actual, Xm, Xk, Xl)
+            print('FASE DE CRUZAMIENTO: x => %f | y => %f | a => %f | b => %f' % (cr_x, cr_y, cr_a, cr_b))
+            vector_trial = cruzamiento(poblacion_actual, vector_muts, j, cr_x, cr_y, cr_a, cr_b)
+            poblacion_nueva.append(compare(poblacion_actual, vector_trial, j))
+        print('NUEVA POBLACIÓN')
+        poblacion_actual = poblacion_nueva
+        imprimir(poblacion_nueva, aptitudes(poblacion_nueva))
+        print('\n\n')
+        # poblacion_actual = poblacion_nueva
+        # imprimir(poblacion_actual)
