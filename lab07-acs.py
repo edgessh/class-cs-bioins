773c7bbdfb49c91d2fba8890f28fd1ef5de7a763
lewistec90 <gitlmcs@gmail.com>
2020-06-16 13:06:41 -0500

    lab07 ant colony system

---
+++ lab07-acs.py
@@ -0,0 +1,11 @@
+# Muestre las dos primeras iteraciones de un Sistema de Colonia de Hormigas (Ant Colony System) para resolver el
+# siguiente problema TSP:
+# • Feromona inicial: 0.1
+# • Cantidad de Hormigas: 4
+# • α = 1
+# • β = 1
+# • ρ = 0:01
+# • Q = 1:0
+# • Considere una ciudad inicial para todas las hormigas.
+# • Por lo menos considere 1 decimal y como máximo 3 decimales.
+
